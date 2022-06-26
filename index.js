2f7928bca3d92b00a13635fe611d7b1b8c3d643e
Elena Diachkova <zabava1711@bk.ru>
2022-06-26 15:56:00 +0300

    index , package

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 import _ from "lodash";
-console.log(_.last(['one', 'two']));
-console.log('Hello, Hexlet!');
+console.log(_.last(["one", "two"]));
+console.log("Hello, Hexlet!");
+const fn = (a, b) => {
+  const c = a + b;
+  return c;
+};
